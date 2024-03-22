187b9554b3c0075806eb9d0d67d4bb405c4d70a7
LuisJaSe <luis.jaramillo.sevilla@gmail.com>
2024-03-22 10:50:40 -0500

    Agregando graficas a proyecto

---
+++ graficas/graficas.py
@@ -0,0 +1,16 @@
+# Importando la libreria de matplotlib
+import matplotlib.pyplot as plt
+
+# Ejemplo de grafica pastel utilziando matplotlib
+def grafica_pastel():
+    labels = ["A", "B", "C", "D", "E", "F"]
+    values = [20, 30, 40, 50, 60, 200]
+    
+    fig, ax = plt.subplots() # Crear figura y eje
+    ax.pie(values, labels=labels) # Generar grafica de torta
+    ax.axis('equal') # Hacer que la grafica sea circular
+    #pyplot.show() # Mostrar grafica
+    plt.savefig("imagen_grafica.png")
+    plt.close()
+    
+grafica_pastel()
---
+++ graficas/main.py
@@ -0,0 +1,10 @@
+# Importar el modulo de la grafica en main
+import graficas
+
+def run():
+    graficas.grafica_pastel()
+    
+run()
+
+if __name__ == '__main__':
+    run()
